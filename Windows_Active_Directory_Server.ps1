78a53123285f09f1e2bf8158a330bdcfb02de0e3
mds5648 <54720730+mds5648@users.noreply.github.com>
2021-09-01 11:38:07 -0400

    Intial

---
+++ Windows_Active_Directory_Server.ps1
@@ -0,0 +1,13 @@
+﻿$DNS_IP_ADDRESS = "10.10.10.1"
+<#
+The rule is deigned to allow ingress traffic using the LDAPS protcol, which provides encryption that is not on a normal 
+LDAP protocol that exists on port 389.
+#>
+New-NetFirewallRule -DisplayName "LDAPS" -Direction Inbound -LocalPort 636 -Protocol LDAPS -Action Allow
+
+<#
+
+#>
+New-NetFirewallRule -DisplayName "DNS" -Direction Inbound -LocalPort 53 -LocalAddress $DNS_IP_ADDRESS -Protocol TCP,UDP -Action Allow
+
+New-NetFirewallRule -DisplayGroup "Group Policy" -Direction Outbound -Action Allow
